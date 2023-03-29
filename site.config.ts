48c95421905672d90224a8e2e5762084418ac27f
Kai Xie <kxie.cs@gmail.com>
2023-03-30 00:53:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -55,11 +55,11 @@ export default siteConfig({
   navigationLinks: [
     {
       title: 'About',
-      pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+      pageId: '193e2cc8b94d46ce8581049e3d468358'
     },
     {
       title: 'Contact',
-      pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+      pageId: '193e2cc8b94d46ce8581049e3d468358'
     }
   ]
 })
